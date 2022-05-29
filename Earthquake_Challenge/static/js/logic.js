e1e70083d3e2c11d34e34496ea3308ebfeebc54c
chris.weber <chris.weber@nerdery.com>
2022-05-29 14:45:52 -0500

    Commit 13-5-3

--- Earthquake_Challenge/static/js/logic.js
+++ Earthquake_Challenge/static/js/logic.js
@@ -2,31 +2,19 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+let map = L.map('mapid').setView([30, 30], 2);
 
-// An array containing each city's location, state, and population.
-let cityData = cities
+L.geoJSON(geojsonFeature).addTo(map);
 
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-    console.log(city)
-    L.marker(city.location)
-        .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population + "</h3>")
-        .addTo(map);
-});
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport, {
+    // We turn each feature into a marker on the map.
+    pointToLayer: function(feature, latlng) {
+      console.log(feature);
+      return L.marker(latlng).bindPopup("<h2>" + feature.properties.city + "</h2>");
+    }
 
-// Coordinates for each point to be used in the polyline.
-let line = [
-  [33.9416, -118.4085],
-  [37.6213, -122.3790],
-  [40.7899, -111.9791],
-  [47.4502, -122.3088]
-];
-
-// Create a polyline using the line coordinates and make the line red.
-L.polyline(line, {
-  color: "yellow"
-}).addTo(map);
+  }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
